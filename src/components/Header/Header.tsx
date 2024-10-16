f96645308eae6a2473c656809b5c0cfb6037060a
Bharath <bharathj222000@gmail.com>
2024-10-16 21:38:56 +0530

    fix build issue

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -41,7 +41,7 @@ const Header = () => {
             direction={{ xs: "column", md: "row" }}
             spacing={4}
             alignItems={{ xs: "flex-start", md: "center" }}
-            className={[styles.navlinks, menuOpen && styles.active]}
+            className={[styles.navlinks, menuOpen && styles.active].filter(Boolean).join(" ")}
             pt={{ xs: 12, md: 1 }}
             pb={{ xs: 4, md: 1 }}
             px={{ xs: 4, md: 0 }}
